28b840c5da5378b5909485bdedc997f2d135b543
mafasir <maharjansachin94@gmail.com>
2024-09-05 07:41:41 +0545

    Project added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    const num=5;
+  return (
+    <>
+      <h1>Navbar</h1>
+      <p>This is Navbar</p>
+    </>
+  )
+}
+
+export default Navbar
